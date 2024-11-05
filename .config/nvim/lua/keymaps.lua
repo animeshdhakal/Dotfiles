916042db552a662e11fc5b0466b4709afc052138
Animesh Dhakal <dhakalanimesh385@gmail.com>
2024-11-05 11:31:52 +0545

    Added i3wm

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -6,7 +6,7 @@ vim.g.netrw_browse_split = 0
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set({"n", "v"}, "<leader>P", "\"+p")
+vim.keymap.set({"n", "v"}, "<leader>v", "\"+p")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -25,20 +25,10 @@ vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
 vim.keymap.set("n", "<leader>b", ":Neotree focus<CR>")
 
--- Easy Split
-vim.keymap.set("n", "<leader>v", ":vsplit<CR>")
-vim.keymap.set("n", "<leader>s", ":split<CR>")
-
 -- Buffer Navigation
 vim.keymap.set("n", "<Tab>", ":bn<CR>")
 vim.keymap.set("n", "<S-Tab>", ":bp<CR>")
 vim.keymap.set("n", "<leader>d", ":bd!<CR>")
-
--- Split Navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
 
 -- Solit Size
 vim.keymap.set("n", "<C-Left>", ":vertical resize -3<CR>")
@@ -47,13 +37,13 @@ vim.keymap.set("n", "<C-Up>", ":horizontal resize -3<CR>")
 vim.keymap.set("n", "<C-Down>", ":horizontal resize +3<CR>")
 
 -- Automatically close brackets
-vim.keymap.set("i", "'", "''<left>")
-vim.keymap.set("i", "\"", "\"\"<left>")
-vim.keymap.set("i", "(", "()<left>")
-vim.keymap.set("i", "[", "[]<left>")
-vim.keymap.set("i", "{", "{}<left>")
-vim.keymap.set("i", "{;", "{};<left><left>")
-vim.keymap.set("i", "/*", "/**/<left><left>")
+-- vim.keymap.set("i", "'", "''<left>")
+-- vim.keymap.set("i", "\"", "\"\"<left>")
+-- vim.keymap.set("i", "(", "()<left>")
+-- vim.keymap.set("i", "[", "[]<left>")
+-- vim.keymap.set("i", "{", "{}<left>")
+-- vim.keymap.set("i", "{;", "{};<left><left>")
+-- vim.keymap.set("i", "/*", "/**/<left><left>")
 
 -- Comments
 vim.keymap.set({"n", "v"}, "<leader>c", "gc", {remap = true})
--- cp-files.sh
+++ cp-files.sh
@@ -4,4 +4,5 @@ cp ~/.config/nvim ./.config -r
 cp ~/.tmux.conf .
 cp ~/.bash_custom .
 cp ~/.clang-format .
-
+cp ~/.config/i3 ./.config -r
+cp ~/.config/i3status ./.config -r
