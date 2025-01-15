8dabff60a7ca7fcf0ae321bf348224cd4b8babc2
Animesh Dhakal <dhakalanimesh385@gmail.com>
2025-01-15 15:41:54 +0545

    Fixed Deprecation Errors

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,9 +5,6 @@ return {
         configs.setup({
             ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "javascript", "html", "tsx" },
             sync_install = false,
-            autotag = {
-                enable = true
-            },
             highlight = { enable = true },
         })
     end
