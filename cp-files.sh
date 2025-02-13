330c3f006f1f489f587942b230538e2e95f5edcb
Animesh Dhakal <dhakalanimesh385@gmail.com>
2025-02-13 15:24:25 +0545

    Added Case Insensitive Tab

--- cp-files.sh
+++ cp-files.sh
@@ -5,6 +5,7 @@ cp ~/.tmux.conf .
 cp ~/.bash_custom .
 cp ~/.clang-format .
 cp ~/.config/i3 ./.config -r
+cp ~/.inputrc .
 cp ~/.config/zathura ./.config -r
 cp ~/.config/i3status ./.config -r
 cp ~/.wezterm.lua .
